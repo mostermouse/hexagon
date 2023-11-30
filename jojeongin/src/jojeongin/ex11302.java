0ab1b3123fef9088e918495a9a924f0706e64824
헥사곤 <@DESKTOP-P82TD94>
2023-11-30 15:05:06 +0900

    별만들기

---
+++ jojeongin/src/jojeongin/ex11302.java
@@ -0,0 +1,18 @@
+package jojeongin;
+
+public class ex11302 {
+
+	public static void main(String[] args) {
+	for(int i = 1; i< 10; i+=2) {
+		for(int j = 9; j>i; j-=2) {
+			System.out.print(" ");
+		}
+		for(int j = 0; j <i; j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+
+	}
+
+}
